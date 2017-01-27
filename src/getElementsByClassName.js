d54cec3aca37837c8aad95d911e14d815367f8c4
Tayo <ojolao@gmail.com>
2017-01-27 07:26:01 -0500

    wrote stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,24 @@
 //   return document.getElementsByClassName(className);
 // };
 
+/* You should use document.body, element.childNodes, and element.classList */
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  //define output
+  //search through the document.body.childNodes
+  var output = [];
+  function search(node){
+  	if(node.classList){
+  		if(node.classList.contains(className)){
+  		output.push(node);
+  	}
+  }
+  	node.childNodes.forEach(function(child){
+  		search(child);
+  	});
+  }
+  search(document.body);
+  return output; 
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //return typeOf and process, holding out for arrays and objects
+  var toType = function(x) {
+  	return ({}).toString.call(x).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+  }
+
+/* - Number-return obj
+- String- " + obj + "
+- Boolean
+- Array
+- Object
+- Function- null
+- Null- null
+- Undefined - null */
+  if(toType(obj) === 'boolean'){
+  	return obj + '';
+  } 
+  if(toType(obj) === 'null' || toType(obj) === 'function' || toType(obj) === 'undefined'){
+  	return null + '';
+  }
+  if(toType(obj) === 'string'){
+  	return '"' + obj + '"';
+  }
+  if(toType(obj) === 'number'){
+  	return obj + '';
+  }
+  if(toType(obj) === 'array'){
+  	return '[' + obj.map(function(item){
+  		return stringifyJSON(item);
+  	}).join(',') + ']';
+  }
+  if(toType(obj) === 'object'){
+  	var output = [];
+  	Object.keys(obj).forEach(function(key){
+  		var stringed = stringifyJSON(obj[key]);
+  		if(typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function'){
+  			output.push('"' + key + '":' + stringed);
+  		}
+  	});
+  	return '{' + output.join(',') + '}';
+  }
 };
